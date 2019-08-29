77ea730f767eb0f0fd132fb647433b5ccc3463e5
Sean Phillips <loldrg87@gmail.com>
2019-08-29 14:02:40 -0400

    Update testTableRef.js

--- js/testTableRef.js
+++ js/testTableRef.js
@@ -1,1 +1,19 @@
 
+function GetLootFormula(challengeRating,d100){
+  
+  var table = [];
+  var object = {ArtAndGemsGP:"", MagicItemDice:"", MagicItemTable:"", MagicItemDice2:"", MagicItemTable2:""};
+  var i;
+  
+    object.ArtAndGemsGP="1";
+    table.push(object);
+
+    object.ArtAndGemsGP="2";
+    table.push(object);
+
+    console.log(table[0].ArtAndGemsGP);
+  
+    return table[0].ArtAndGemsGP;
+}
+
+
